75a0bdf13cade9398f0f63221bc52bc031b90b7e
CuberLuo <1501858187@qq.com>
2023-05-09 13:54:54 +0800

    解决WebView刷新问题

--- app/src/main/java/cn/ikun/chatgpt/MainActivity.java
+++ app/src/main/java/cn/ikun/chatgpt/MainActivity.java
@@ -1,12 +1,16 @@
 package cn.ikun.chatgpt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         WebView webView = findViewById(R.id.webview);
         webView.loadUrl("http://1.15.134.164:999");
+        webView.setWebViewClient(new WebViewClient() {
+            //重载链接跳转,防止跳转到浏览器
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Uri uri = request.getUrl();
+                Log.i("Test",uri.toString());
+                if(uri.toString().equals("https://github.com/CuberLuo/ikunGPT-Plus")){
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }else{
+                    view.reload();
+                }
+                return true;
+            }
+        });
+
         WebSettings webSettings=webView.getSettings();
         //启用js
         webSettings.setJavaScriptEnabled(true);
         //启用localStorage
         webSettings.setDomStorageEnabled(true);
+
+        //webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+
     }
+
 }
