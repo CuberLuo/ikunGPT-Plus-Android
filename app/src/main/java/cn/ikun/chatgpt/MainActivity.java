294519bbb710035a337181346e27fff0eeb083d1
CuberLuo <1501858187@qq.com>
2023-06-21 15:39:51 +0800

    url更新,解决启动白屏问题

--- app/src/main/java/cn/ikun/chatgpt/MainActivity.java
+++ app/src/main/java/cn/ikun/chatgpt/MainActivity.java
@@ -2,6 +2,7 @@ package cn.ikun.chatgpt;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,14 +21,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = findViewById(R.id.webview);
-        webView.loadUrl("http://1.15.134.164:999");
+        //背景设置为黑色，解决加载前白屏问题
+        webView.setBackgroundColor(Color.parseColor("#000000"));
+        webView.loadUrl("http://47.115.203.9:12345");
         webView.setWebViewClient(new WebViewClient() {
             //重载链接跳转,防止跳转到浏览器
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 Uri uri = request.getUrl();
-                Log.i("Test",uri.toString());
-                if(!uri.toString().contains("http://1.15.134.164:999")){
+                //Log.i("Test",uri.toString());
+                if(!uri.toString().contains("http://47.115.203.9:12345")){
                     Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                     startActivity(intent);
                 }else{
